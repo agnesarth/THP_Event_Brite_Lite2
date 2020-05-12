320cdbf03e66e63f711c1bb715ba1742832a9bc6
Raphael-dln <delannay.raphael@gmail.com>
2020-05-12 13:15:55 +0200

    update mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+ # before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
