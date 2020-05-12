6e8fb683d1aa3b86c66ccf2a9bb6d350d205c9d1
Raphael-dln <delannay.raphael@gmail.com>
2020-05-12 20:13:22 +0200

    index + user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- # before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -9,17 +9,45 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    if !current_user?(@user.id)
+      flash[:error] = "You are the wrong user."
+      redirect_to root_path
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+    if !current_user?(@user.id)
+      flash[:error] = "You are the wrong user."
+      redirect_to root_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if !current_user?(@user.id)
+      flash[:error] = "You are the wrong user."
+      redirect_to root_path
+    else
+      if @user.update(user_params)
+        flash[:success] = "Your profile has been updated."
+        redirect_to user_path(@user.id)
+      else
+        render 'edit'
+      end
+    end
   end
 
-  def destroy
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
   end
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'https://pacific-springs-71389.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
